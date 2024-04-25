d077cfbfe09ff4965122310ea237c28f58e3ca15
KIM-Insu <123820557+KIM-Insu@users.noreply.github.com>
2024-04-25 18:20:38 +0900

    Update 햄버거 만들기.py

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,11 +1,11 @@
 def solution(ingredient):
     answer = 0
-    burger = []
+    burger = []                          # stack 생성
 
-    for i in ingredient:
-        burger.append(i)
-        if burger[-4:] == [1, 2, 3, 1]:
-            answer += 1
-            for _ in range(4):
+    for i in ingredient:    
+        burger.append(i)     
+        if burger[-4:] == [1, 2, 3, 1]:  # stack에 원소를 push하다가 스택의 마지막 4개의 값이 (빵(1), 야채(2), 고기(3), 빵(1)) 순으로 쌓이면 
+            answer += 1                    
+            for _ in range(4):           # 결과값에 버거 1개를 더하고 4개의 원소를 pop 
                 burger.pop()
     return answer
