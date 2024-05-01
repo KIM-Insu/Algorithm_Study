34bb4c5d3dde7cfc944f5f52f61455c340614256
KIM-Insu <123820557+KIM-Insu@users.noreply.github.com>
2024-05-01 18:23:39 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,22 +1,16 @@
 def solution(progresses, speeds):
-    done = []
-    for i,j in zip(progresses, speeds):
-        if (100-i) % j == 0:
-            done.append((100-i) // j)
-        else:
-            done.append(((100-i) // j) + 1)
-    
+    done = [(100-i) // j + min((100-i) % j, 1) for i,j in zip(progresses, speeds)]
     stack = []
     result = []
+    
     cnt = 1
     for i in done:
         if stack and stack[-1] < i:
-            stack.pop()
             stack.append(i)
             result.append(cnt)
             cnt = 1
         elif not stack:
-            stack.append(i)
+            stack.append(i)    
         else:
             cnt += 1
     result.append(cnt)
