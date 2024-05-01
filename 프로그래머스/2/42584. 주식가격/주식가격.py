df586a4844dc6a10f0af4150790887671b86cb50
KIM-Insu <123820557+KIM-Insu@users.noreply.github.com>
2024-05-01 15:51:04 +0900

    Update 주식가격.py

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -10,24 +10,3 @@ def solution(prices):
                 break
         result.append(cnt)
     return result
-
-# def solution(prices):
-#     stack = []
-#     result = [0] * len(prices)
-
-#     for i in range(len(prices)):
-#         days = 1
-#         repeat = 1
-#         if stack:
-#             while stack and stack[-1] > prices[i]:
-#                 stack.pop()
-#                 result[i-repeat] = days
-#                 repeat += 1
-#                 days += 1
-
-#         stack.append(prices[i])
-            
-#     for j in range(len(result)-1):
-#         if result[j] == 0:
-#             result[j] = len(result) - j - 1
-#     return result
