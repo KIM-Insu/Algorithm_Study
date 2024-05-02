5539ca1a0f7ff4007a3aa87673b4f218d890c536
KIM-Insu <123820557+KIM-Insu@users.noreply.github.com>
2024-05-03 03:06:45 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,9 @@
 def solution(arr):
     stack = []
     for i in arr:
-        if stack and stack[-1] == i:
+        if stack and stack[-1] == i:  # stack이 비어있지 않고 stack의 마지막 원소와 현재 글자가 같으면 (= 중복되는 숫자면) continue
             continue
-        else:
+        else:                         # stack이 비어있거나 stack의 마지막 원소와 현재 글자가 다르면 (=처음 나오는 숫자면) stack에 append   
             stack.append(i)
 
     return stack
