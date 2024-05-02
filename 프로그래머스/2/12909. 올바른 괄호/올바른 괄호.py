89eaac9c984198552a392b27cfdabd3dfc42bee1
KIM-Insu <123820557+KIM-Insu@users.noreply.github.com>
2024-05-03 02:59:40 +0900

    Update 올바른 괄호.py

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,11 +1,13 @@
 def solution(s):
     stack = []
     for i in s:
-        if i == ')':
-            if stack and stack[-1] == '(':
+        if i == ')':                        # ')'가 나왔다면
+            if stack and stack[-1] == '(':      # 1. stack이 비어있지 않고 stack의 마지막 원소가 '('이라면 pop해서 제거
                 stack.pop()
-            else:
-                stack.append(i)
-        else:
-            stack.append(i)
+            
+            else:                               # 2. stack이 비어있을 때 = 왼쪽 괄호보다 오른쪽 괄호가 먼저 나온 경우 stack에 append
+                stack.append(i)                 # 3. stack의 마지막 원소가 ')' = 오른쪽 괄호가 연속으로 나온 경우 stack에 append
+                                            
+        else:                               # '('가 나왔다면
+            stack.append(i)                     # stack에 append  
     return not stack
