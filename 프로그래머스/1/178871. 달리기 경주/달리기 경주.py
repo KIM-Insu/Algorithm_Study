e37a7dc64b73bedc23d4c611b829acfec70d78fe
KIM-Insu <123820557+KIM-Insu@users.noreply.github.com>
2024-05-06 17:20:51 +0900

    Update 달리기 경주.py

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,12 +1,10 @@
 def solution(players, callings):
-    rank = {val : idx for idx, val in enumerate(players)}
+    rank = {val : idx for idx, val in enumerate(players)}     # 실시간 달리기 순위 저장하는 딕셔너리 생성
+    for i in callings:                                        
+        called = rank[i]                                      # 추월한 선수의 기존 순위 저장  
     
-    for i in callings:
-        called = rank[i]
+        rank[players[called - 1]] += 1                        # 추월당한 선수의 순위 +1
+        rank[i] -= 1                                          # 추월한 선수의 순위 -1
+        players[called - 1], players[called] = players[called], players[called - 1]  # 추월한 선수와 추월 당한 선수의 원소 순서를 업데이트     
     
-        rank[players[called - 1]] += 1
-        rank[i] -= 1
-        
-        players[called - 1], players[called] = players[called], players[called - 1]
-        
     return players
