0b19261f198a72a22bae8bf31398fb27b4bbe405
KIM-Insu <123820557+KIM-Insu@users.noreply.github.com>
2024-05-14 21:44:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.63 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,27 +1,35 @@
 from collections import deque
+
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
+    row = len(maps)
+    col = len(maps[0])
     
-    dx = [-1, 1, 0, 0]
+    # 상하좌우 이동
+    dx = [1, -1, 0, 0]
     dy = [0, 0, -1, 1]
     
-    queue = deque()
-    queue.append((0,0))
+    # 방문 여부와 이동 거리를 저장하는 배열
+    visited = [[0 for _ in range(col)] for _ in range(row)]
+    
+    queue = deque([(0, 0)])
     
     while queue:
         x, y = queue.popleft()
+        visited[x][y] = 1
+        
         for i in range(4):
+            # 상하좌우로 이동
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if nx <= -1 or nx >= n or ny <= -1 or ny >= m:
-                continue
-            
-            if maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
-                queue.append((nx, ny))
-    
-    if maps[n-1][m-1] > 1:
-        return maps[n-1][m-1]
-    else: return -1
+            # nx, ny가 배열 범위 안이고
+            if 0 <= nx < row and 0 <= ny < col:
+                # maps가 1이고, 방문한 적이 없을(-1) 때
+                if maps[nx][ny] == 1 and visited[nx][ny] == 0:
+                    maps[nx][ny] = maps[x][y] + 1
+                    queue.append((nx, ny)) # 지금 방문한 곳에서 
+        
+    if visited[row-1][col-1]:
+        return maps[row-1][col-1]
+    else:
+        return -1
