1dd520c035881a51f559e1b6b2b49fa541c72b84
KIM-Insu <123820557+KIM-Insu@users.noreply.github.com>
2024-05-20 18:10:46 +0900

    [Silver I] Title: 점프왕 쩰리 (Large), Time: 68 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.py
+++ 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.py
@@ -1,16 +1,14 @@
 from collections import deque
 
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * n for _ in range(n)]
+
+dx = [0, 1]
+dy = [1, 0]
+
 def bfs(x, y):
-    n = int(input())
-    graph = [list(map(int, input().split())) for _ in range(n)]
-    visited = [[0] * n for _ in range(n)]
-    
-    # 아래, 오른쪽 이동
-    dx = [0, 1]
-    dy = [1, 0]
-    
-    queue = deque([[x,y]])
-    
+    queue = deque([[x, y]])
     while queue:
         x, y = queue.popleft()
         present = graph[x][y]
@@ -22,13 +20,9 @@ def bfs(x, y):
             nx = x + dx[i] * present
             ny = y + dy[i] * present
             
-            # nx, ny가 범위 안이고
-            if 0 <= nx < n and 0 <= ny < n:
-                # 방문한 적이 없을 때
-                if visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
-                    queue.append([nx, ny])
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                queue.append([nx, ny])
 
-if bfs(0,0):
-    print('HaruHaru')
+if bfs(0,0): print('HaruHaru')
 else: print('Hing')
