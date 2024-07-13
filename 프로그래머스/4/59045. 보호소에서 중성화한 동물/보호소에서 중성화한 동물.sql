137438459bb7020baaca671a76135e8419665f1f
KIM-Insu <123820557+KIM-Insu@users.noreply.github.com>
2024-07-13 23:41:14 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
 SELECT i.animal_id, i.animal_type, i.name
 from animal_ins i inner join animal_outs o on i.animal_id = o.animal_id
-where i.sex_upon_intake in ('Intact Male', 'Intact Female')
-and o.sex_upon_outcome in ('Neutered Male', 'Spayed Female')
+where i.sex_upon_intake like 'Intact%'
+and o.sex_upon_outcome not like 'Intact%'
 order by 1
